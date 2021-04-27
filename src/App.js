7b65ff6c8fa28f73f22583760456018f933a58d9
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-04-27 19:10:47 +0530

    agaain

--- src/App.js
+++ src/App.js
@@ -1,55 +1,33 @@
+import { useState } from 'react';
 import './App.css';
+import MenuIcon from '@material-ui/icons/Menu';
+import ClearIcon from '@material-ui/icons/Clear';
 
 function App() {
 
-  const data = [
-    {
-      "name":"raghav",
-      "age":21,
-    },
-    {
-      "name":"muskaan",
-      "age":18,
-    },
-    {
-      "name":"john",
-      "age":25,
-    },
-    {
-      "name":"sikander",
-      "age":27,
-    },
-    {
-      "name":"anjali",
-      "age":31,
-    },
-    {
-      "name":"Aayush",
-      "age":11,
-    },
-    {
-      "name":"yuji",
-      "age":61,
-    },
-    {
-      "name":"naruto",
-      "age":31,
-    },
-    {
-      "name":"hinata",
-      "age":29,
-    },
-    {
-      "name":"might guy",
-      "age":41,
-    }
-  ];
-
-  console.log(data.length)
+  const [ showMenu, setShowMenu ] = useState(true);
 
   return (
     <div className="App">
-      
+      <h2 className='navbar__heading' > navbar </h2>
+
+      <ul className={showMenu ? 'navbar__menuItems' : 'navbar__menuItems active' } >
+        <li>
+          Home
+        </li>
+
+        <li>
+          About 
+        </li>
+
+        <li>
+          Contact us
+        </li>
+      </ul>
+
+      <div className='navbar__menuIcon' onClick={() => setShowMenu(!showMenu)} >
+        {showMenu ? <MenuIcon /> : <ClearIcon  />}
+      </div>
     </div>
   );
 }
